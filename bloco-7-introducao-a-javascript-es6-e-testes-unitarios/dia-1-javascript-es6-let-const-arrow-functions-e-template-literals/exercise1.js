a76f002189523f9e6916ec4348651417e8e2ca02
renanbigm <renanbigm@gmail.com>
2022-05-08 00:08:55 -0300

    exercicio 2.2 completo

--- bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercise1.js
+++ bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercise1.js
@@ -9,13 +9,14 @@ const testingScope = (escopo) => {
   }
 }
 
-
-
-
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-const returnSortedArr = () => oddsAndEvens.sort((a, b) => a - b);
-
-// console.log(`Os números ${returnSortedArr()} se encontram ordenados de forma crescente`);
+const oddsAndEvens = [3, 4, 7, 2, 15, 17, 21];
+const returnSortedArr = () => oddsAndEvens.sort((a, b) => b - a);
+for (let i = 0; i < oddsAndEvens.length; i += 1) {
+  if (oddsAndEvens[i] < oddsAndEvens[i - 1]) {
+    oddsAndEvens[i], oddsAndEvens[i - 1] = oddsAndEvens[i - 1], oddsAndEvens[i];
+  }
+}
+// console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente`);
 
 // returnFatorial = (n) => (n * (n - 1));
 
@@ -27,4 +28,27 @@ function fatorial(n) {  // 5*4 = 20 * 3 = 60 * 2 = 120 * 1 = 120
   } 
   return fatorialN;
 }
-console.log(fatorial(6));
+
+const fat = (n) => (n === 0) ? 1 : (n * fat(n - 1));
+
+// console.log(fat(6));
+
+// const longestWord = (str) => (str.split()) ?
+
+function longest(str) {
+  const arrStr = str.split(' ');
+  let compare = arrStr[0];
+  for (let i = 0; i < arrStr.length; i += 1) {
+    if (arrStr[i].length > compare.length) {
+      compare = arrStr[i];
+    }
+  }
+  return compare;
+}
+console.log(longest('Antônio foi no banheiro eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee não sabemos o que aconteceu'));
+
+
+
+
+
+
