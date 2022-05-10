f2026234d35a123c98fcdcaf0f4d3554169286ac
renanbigm <renanbigm@gmail.com>
2022-05-10 13:57:08 -0300

    exercise 1.1

--- fluxo-de-exceção-e-Objetos/scrpit.js
+++ fluxo-de-exceção-e-Objetos/scrpit.js
@@ -14,11 +14,27 @@ const student1 = {
   JavaScript: 'Ótimo',
   SoftSkills: 'Ótimo',
 };
+console.log(student1.);
 
 function returnKeysValues(obj) {
   const keys = Object.keys(obj);
   for (let i in keys) {
-  console.log(`${keys[i]}: ${Object.values(obj)[i]}`);
+  console.log(`${Object.keys(obj)[i]}: ${Object.values(obj)[i]}`);
   }
 }
-console.log(returnKeysValues(student1))
+// console.log(returnKeysValues(student1));
+
+const getDayName = (number) => {
+  if (number < 1 || number > 7) throw new Error('Informe um número entre 1 e 7');
+  const dayNumber = number - 1;
+  const days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+  return days[dayNumber];
+};
+
+const printDayname = (dayNumber) => {
+  try {
+    console.log(getDayName(dayNumber));
+  } catch (e) {
+    console.log(e.message);
+  }
+};
