5c654db920161a5950098fed34fe3518eb002423
renanbigm <renanbigm@gmail.com>
2022-05-10 15:29:27 -0300

    exercises 3.1, 3.2 completeds

---
+++ fluxo-de-exceção-e-Objetos/exercise3.js
@@ -0,0 +1,30 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function addInfoToObj(obj, key, value) {
+  return obj[key] = value;
+}
+addInfoToObj(lesson2, 'turno', 'noite');
+// console.log(lesson2);
+
+function returnObjKeysList(obj) {
+  return Object.keys(obj);
+}
+console.log(returnObjKeysList(lesson1))
