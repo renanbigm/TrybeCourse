aa662fb9534618b9eb4819dfc4515d4963630768
renanbigm <renanbigm@gmail.com>
2022-05-10 10:15:06 -0300

    exercicio de fixação 2

--- fluxo-de-exceção-e-Objetos/scrpit.js
+++ fluxo-de-exceção-e-Objetos/scrpit.js
@@ -2,8 +2,23 @@ function generateObj(key, value) {
   const games = {
     online: 'elol',
     local: 'rdr',
-  }
+  };
   games[key] = value;
   return games
 }
-console.log(generateObj('board', 'zombiecide'));
+// console.log(generateObj('board', 'zombiecide'));
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+function returnKeysValues(obj) {
+  const keys = Object.keys(obj);
+  for (let i in keys) {
+  console.log(`${keys[i]}: ${Object.values(obj)[i]}`);
+  }
+}
+console.log(returnKeysValues(student1))
