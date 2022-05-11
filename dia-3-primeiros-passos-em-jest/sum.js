9008dc89c0d4c60a4b3b9214440c38f5efe1ab17
renanbigm <renanbigm@gmail.com>
2022-05-11 14:00:30 -0300

    exercise 1.3 completed

--- dia-3-primeiros-passos-em-jest/sum.js
+++ dia-3-primeiros-passos-em-jest/sum.js
@@ -1,4 +1,9 @@
 // sum.js
-const sum = (a, b) => a + b;
+const sum = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error('Tipo inválido, insira um número do tipo número');
+  }
+  return a + b;
+}
 
 module.exports = sum;
--- dia-3-primeiros-passos-em-jest/sum.test.js
+++ dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,10 +1,13 @@
 const sum = require('./sum');
 
 describe('testando a função sum', () => {
-  it('sums two values', () => {
+  test('sums two values', () => {
     expect(sum(4, 5)).toBe(9);
   });
-  it('sums two values', () => {
+  test('sums two values', () => {
     expect(sum(0, 0)).toBe(0);
+  });
+  test('sums two values', () => {
+    expect(() => sum(4, '6')).toThrowError('Tipo inválido, insira um número do tipo número');
   });
 });
