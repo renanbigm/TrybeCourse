c039b9ec8761acb5e09b8db35bd75a8d628fce7c
renanbigm <renanbigm@gmail.com>
2022-05-13 21:42:31 -0300

    exercise 2 completed

--- dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
+++ dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
@@ -1,8 +1,23 @@
+//exercicse 1
 const returnaFullNameAndEmail = (name) => {
   const email = `${name.split(' ').join('_').toLowerCase()}@trybe.com`;
   return `{ ${name}, email:${email} }`
 }
 const newEmployees = (returnaFullNameAndEmail) => ({ id: returnaFullNameAndEmail('Renan Fagundes') })
 
-console.log(newEmployees(returnaFullNameAndEmail));
+// console.log(newEmployees(returnaFullNameAndEmail));
 
+//exercise 2
+const checkNumber = (chosenNum, drawnNum) => chosenNum === drawnNum;
+
+const getRandomNumber = (chosenNum, checkNumber) => {
+  const drawnNum = Math.floor(Math.random() * 5) + 1;
+  console.log(drawnNum);
+  if (checkNumber(chosenNum, drawnNum)) {
+    return 'Parabéns você ganhou';
+  }
+  return 'Tente novamente';
+}
+// console.log(getRandomNumber(3, checkNumber))
+
+//exercise 3
