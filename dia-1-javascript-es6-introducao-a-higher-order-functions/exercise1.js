2b476b450bfe30ff0df4371160e8c2f8b501ab10
renanbigm <renanbigm@gmail.com>
2022-05-14 17:46:12 -0300

    exercise 3 completed

--- dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
+++ dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
@@ -3,9 +3,9 @@ const returnaFullNameAndEmail = (name) => {
   const email = `${name.split(' ').join('_').toLowerCase()}@trybe.com`;
   return `{ ${name}, email:${email} }`
 }
-const newEmployees = (returnaFullNameAndEmail) => ({ id: returnaFullNameAndEmail('Renan Fagundes') })
+const newEmployees = () => ({ id: returnaFullNameAndEmail('Renan Fagundes') })
 
-// console.log(newEmployees(returnaFullNameAndEmail));
+console.log(newEmployees());
 
 //exercise 2
 const checkNumber = (chosenNum, drawnNum) => chosenNum === drawnNum;
@@ -21,3 +21,20 @@ const getRandomNumber = (chosenNum, checkNumber) => {
 // console.log(getRandomNumber(3, checkNumber))
 
 //exercise 3
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkAnswers = (rightAnswers, studentAnswers) => {
+  let testResult = 0;
+  for (let i = 0; i < studentAnswers.length; i += 1) {
+    if (studentAnswers[i] === rightAnswers[i]) testResult += 1;
+    if (studentAnswers[i] !== rightAnswers[i] && studentAnswers[i] !== 'N.A') testResult -= 0.5;
+  }
+  return testResult;
+}
+
+const avaliation = (rightAnswers, studentAnswers, callback) => {
+  return callback(rightAnswers, studentAnswers);
+};
+
+console.log(avaliation(rightAnswers, studentAnswers, checkAnswers))
