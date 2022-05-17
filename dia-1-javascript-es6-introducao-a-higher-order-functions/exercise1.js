895f9fd7cfaecc2f2c909e728f2d5563fbd0aa5a
renanbigm <renanbigm@gmail.com>
2022-05-17 12:37:01 -0300

    finished

--- dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
+++ dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
@@ -82,7 +82,7 @@ function smallerName() {
 function getNamedBook() {
   return books.find((element) => element.name.length === 26);
 }
-// console.log(getNamedBook())
+console.log(getNamedBook())
 
 //exercise 4
 function booksOrderedByReleaseYearDesc() {
@@ -111,9 +111,64 @@ function authorUnique() {
     return true;
   });
 }
-console.log(authorUnique())
+// console.log(authorUnique())
 
 // return books.every((book) =>
 // !books.some((bookSome) =>
 //   (bookSome.author.birthYear === book.author.birthYear)
 //   && (bookSome.author.name !== book.author.name)));
+const objPeople = [
+  { name: 'José', age: 21 },
+  { name: 'Lucas', age: 19 },
+  { name: 'Maria', age: 16 },
+  { name: 'Gilberto', age: 18 },
+  { name: 'Vitor', age: 15 },
+];
+
+const verifyAgeDrive = (arrayOfPeople) => (
+  arrayOfPeople.filter((people) => (people.age < 18))
+);
+
+// console.log(verifyAgeDrive(objPeople));
+// [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+  listStudents.filter((student) => student !== name);
+  // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+// console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const productsList = (products, prices) => products.map((element, index) => { element[index]: prices[index]})
+
+
+
+
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
