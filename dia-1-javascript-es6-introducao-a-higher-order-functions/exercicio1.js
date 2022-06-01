4a7d15fdbbb7760851e56584de2f93d9da0baa71
renanbigm <renanbigm@gmail.com>
2022-06-01 09:10:01 -0300

    iniciando exercicios dia 1 de front-end

--- dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
+++ dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
@@ -135,3 +135,7 @@ const yearSeasons = {
   autumn: ['September', 'October', 'November'],
   winter: ['December', 'January', 'February'],
 };
+const { spring, summer, autumn, winter } = yearSeasons;
+const months = [...spring, ...summer, ...autumn, ...winter];
+
+console.log(months);
