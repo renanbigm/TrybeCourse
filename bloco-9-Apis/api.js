92500fbf37f66dece27bb9787dd2b4c8c549590e
renanbigm <renanbigm@gmail.com>
2022-06-02 11:05:58 -0300

    exercicio 3 completo

---
+++ bloco-9-Apis/api.js
@@ -0,0 +1,19 @@
+// const fetch = require('node-fetch');
+
+const showCoins = (coins) => {
+  const ul = document.querySelector('#criptoslist');
+  coins.forEach((element) => {
+    const li = document.createElement('li');
+    li.innerText = element.symbol;
+    ul.appendChild(li); 
+  });
+};
+
+const getCoins = () => {
+  const url = 'https://api.coincap.io/v2/assets';
+  fetch(url)
+    .then(response => response.json())
+    .then(data => showCoins(data.data));
+};
+
+getCoins();
