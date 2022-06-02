ba9a4957efb1fbcd7a2f2ade5e6525fa1450a78d
renanbigm <renanbigm@gmail.com>
2022-06-02 16:05:44 -0300

    finalizados

--- bloco-9-Apis/api.js
+++ bloco-9-Apis/api.js
@@ -1,11 +1,19 @@
-// const fetch = require('node-fetch');
+const getPrices = () => {
+  const url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest//currencies/usd.min.json';
+  const brl = fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.usd.brl)
+    .catch((error) => console.log(`Parametro inválido ${error}`));
+  return brl;
+};
 
-const showCoins = (coins) => {
+const showCoins = async (coins) => {
+  const prices = await getPrices();
   const ul = document.querySelector('#criptoslist');
   coins.forEach((element, index) => {
     const li = document.createElement('li');
     if(index < 10) {
-      li.innerText = element.symbol;
+      li.innerText = `${element.symbol}: ${Math.round(element.priceUsd * prices)}`;
       ul.appendChild(li);
     }
   });
@@ -14,9 +22,10 @@ const showCoins = (coins) => {
 const getCoins = () => {
   const url = 'https://api.coincap.io/v2/assets';
   fetch(url)
-    .then(response => response.json())
-    .then(data => showCoins(data.data))
+    .then((response) => response.json())
+    .then((data) => showCoins(data.data))
     .catch((error) => console.log(`Parametro inválido ${error}`));
 };
 
 getCoins();
+
