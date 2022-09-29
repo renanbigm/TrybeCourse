d9a534cc996fb5e6eca949c17cbf6eb687e5adad
Renan Fagundes <renan@gmail.com>
2022-09-29 19:34:39 -0300

    exercicio de IMC

--- back-end/introNodeJS/bmi.js
+++ back-end/introNodeJS/bmi.js
@@ -1,1 +1,23 @@
-console.log((60 / (1.70 ** 2)).toFixed(2));
+const readline = require('readline-sync');
+
+function handleBMI(weight, height) {
+  console.log(`Weight: ${weight}, Height: ${height}`);
+
+  const heightInMeters = height / 100;
+  const heightSquared = heightInMeters ** 2;
+
+  const bmi = (weight / heightSquared);
+  
+  return bmi;
+}
+
+function main() {
+   const weight = readline.questionInt('What\'s your weight? (kg) ');
+   const height = readline.questionInt('What\'s your height? (cm) ');
+  
+   const bmi = handleBMI(weight, height);
+  
+  console.log(`BMI: ${bmi.toFixed(2)}`);
+}
+
+main();
