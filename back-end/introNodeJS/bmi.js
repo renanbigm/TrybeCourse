f2250a303cc2bcca392000b13ad5c44c5d327ae5
Renan Fagundes <renan@gmail.com>
2022-09-29 22:47:33 -0300

    exercicio runtime async

--- back-end/introNodeJS/bmi.js
+++ back-end/introNodeJS/bmi.js
@@ -12,12 +12,32 @@ function handleBMI(weight, height) {
 }
 
 function main() {
-   const weight = readline.questionInt('What\'s your weight? (kg) ');
-   const height = readline.questionInt('What\'s your height? (cm) ');
+  const weight = readline.questionFloat('What\'s your weight? (kg) ');
+  const height = readline.questionInt('What\'s your height? (cm) ');
   
-   const bmi = handleBMI(weight, height);
+  const bmi = handleBMI(weight, height);
   
-  console.log(`BMI: ${bmi.toFixed(2)}`);
+  console.log(bmi);
+  switch (bmi) {
+    case bmi < 18,5:
+      console.log('Status: Underweight (thin)');
+      break;
+    case bmi >= 18,5 && bmi < 25:
+      console.log('Status: Normal weight');
+      break;
+    case bmi >= 25 && bmi < 30:
+      console.log('Status: Overweight (overweight)');
+      break;
+    case bmi >= 30 && bmi < 35:
+      console.log('Status: Grade I obesity');
+      break;
+    case bmi >= 35 && bmi < 40:
+      console.log('Status: Grade II obesity');
+      break;  
+    default:
+      console.log(`Status: Obesity grades III and IV`);
+      break;
+  }
 }
 
 main();
