36519a62afded0b7c42dbca26d9cf2d7d4afbfed
Renan Fagundes <renanbigm@gmail.com>
2023-03-13 21:33:34 -0300

    adicionada função de calcular latas por m2

--- python/introducao/do_math.py
+++ python/introducao/do_math.py
@@ -39,3 +39,15 @@ def show_biggest_name(names):
 
 
 print(show_biggest_name(names_list))
+
+
+def paint_costs(area):
+    can_price = 80
+    required_liters = area / 3
+    required_cans = required_liters // 18
+    if required_liters % 18:
+        required_cans += 1
+    return required_cans, required_cans * can_price
+
+
+print(paint_costs(10))
