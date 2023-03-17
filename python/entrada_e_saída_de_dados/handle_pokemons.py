d5330a45eec3c5919df5735979e30da7faf8ea4d
Renan Fagundes <renanbigm@gmail.com>
2023-03-17 16:53:51 -0300

    exercicios do dia, função de printar letras do nome na direção contrária e jogo de acertar palavra

--- python/entrada_e_saída_de_dados/handle_pokemons.py
+++ python/entrada_e_saída_de_dados/handle_pokemons.py
@@ -3,7 +3,7 @@
 
 # with open("./entrada_e_saída_de_dados/pokemons.json") as file:
 #     content = file.read()  # leitura do arquivo
-#     pokemons = json.loads(content)["results"]  # o conteúdo é transformado em estrutura python equivalente, dicionário neste caso.
+#     pokemons = json.loads(content)["results"]  # o conteúdo é transformado estrutura python equivalente, dicionário neste caso.
 #     # acessamos a chave results que é onde contém nossa lista de pokemons
 
 # print(pokemons[0])  # imprime o primeiro pokemon da lista
@@ -36,6 +36,60 @@ grass_type_pokemons = [
 
 
 # abre o arquivo para escrita
-with open("grass_pokemons.json", "w") as file:
+# with open("grass_pokemons.json", "w") as file:
     # escreve no arquivo já transformando em formato json a estrutura
-    json.dump(grass_type_pokemons, file)
+    # json.dump(grass_type_pokemons, file)
+
+
+# def vertical_inverted_ladder(word):
+#     for removed_letters in range(len(word)):
+#         for index in range(len(word) - removed_letters):
+#             print(word[index], end="")
+#         print()
+
+# if __name__ == "__main__":
+#     name = input("Digite um nome: ")
+#     vertical_inverted_ladder(name)
+
+import random
+
+WORDS = [
+    "cat",
+    "elephant",
+    "dog",
+    "monkey",
+    "duck",
+    "chameleon",
+    "bear",
+    "moose",
+    "rooster",
+]
+MAX_ATTEMPTS = 3
+
+
+def draw_secret_word(words):
+    secret_word = random.choice(words)
+    scrambled_word = "".join(random.sample(secret_word, len(secret_word)))
+    return secret_word, scrambled_word
+
+
+def collect_guesses():
+    guesses = []
+    for attempt in range(MAX_ATTEMPTS):
+        guess = input("Guess the word: ")
+        guesses.append(guess)
+    return guesses
+
+
+def check_game_result(secret_word, guesses):
+    if secret_word in guesses:
+        print(f"You win: {secret_word}")
+    else:
+        print(f"You lose: {secret_word}")
+
+
+if __name__ == "__main__":
+    secret_word, scrambled_word = draw_secret_word(WORDS)
+    print(f"Scrambled word is {scrambled_word}")
+    guesses = collect_guesses()
+    check_game_result(secret_word, guesses)
